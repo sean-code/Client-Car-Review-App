6dd0b5917402b5f3ca23c56853db2a4c1a02841e
sean-code <john.nganga@student.moringaschool.com>
2022-12-05 14:07:31 +0300

    Assign a classname to the Navbar and create CSS file to style the Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,18 @@
 import React from "react";
 import {NavLink} from 'react-router-dom';
+import './Navbar.css'
 
 
 function NavBar(){
     return(
         <>
-            CAR REVIEWS
+          <div className="navbar-container">
+            <div className="logo">
+                <h2>CAR REVIEWS</h2>
+            </div>
             <div>
-                <div>
-                    <NavLink to="/">HOME</NavLink>
-                </div>
+                <NavLink to="/">HOME</NavLink>
+            </div>
                 <NavLink to="/signup">SIGN UP</NavLink>
                 <NavLink to="/login">LOG IN</NavLink>                  <NavLink to='/addcar'>ADD CAR </NavLink>
             </div>
