f3ea911d180de7911af25b549d290c33746bd529
sean-code <john.nganga@student.moringaschool.com>
2022-12-05 20:42:49 +0300

    Create a CarPage component to fetch my cars from the backend, and Create a CarCard component to display the Car Details

---
+++ src/components/home/CarCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CarCard(){
+    return(
+        <>
+
+        </>
+    )
+
+}
+
+export default CarCard;
+
---
+++ src/components/home/CarPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CarPage(){
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default CarPage;
