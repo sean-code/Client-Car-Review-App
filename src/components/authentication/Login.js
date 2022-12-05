17c33b655724dc16b315a49434cc50429549bfe0
sean-code <john.nganga@student.moringaschool.com>
2022-12-05 17:17:20 +0300

    Create the login form

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,9 +1,19 @@
 import React from "react";
+import './Authentication.css'
+
 
 function Login(){
     return(
         <div>
-            <h1>Login</h1>
+            <div>
+            <form>
+                <h1>Login</h1>
+                <input type="text" id="username" placeholder="Username"/>
+                <input type="password" id="username" placeholder="Password" />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+
         </div>
     )
 }
