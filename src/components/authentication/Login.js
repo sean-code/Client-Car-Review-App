f8935cc536698701578b965c800f04564efe5947
sean-code <john.nganga@student.moringaschool.com>
2022-12-06 20:28:00 +0300

    create handleSubmit function, add eventListener for log in form(onSubmit and onChange) and add useState

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -6,7 +6,7 @@ function Login({ setUser }){
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    function handleSubmit(event) {
+    function handleSubmit({event}) {
         event.preventDefault();
         fetch("/login", {
             method: "POST",
@@ -16,7 +16,7 @@ function Login({ setUser }){
             body: JSON.stringify({ username, password }),
         }).then((response) => {
           if (response.ok) {
-            response.json().then((user) => setUser(user));
+            response.json().then((user) => setPassword(user));
           }
         });
     }
@@ -24,11 +24,16 @@ function Login({ setUser }){
     return(
         <div>
             <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Login</h1>
-                <input type="text" id="username" placeholder="Username"/>
-                <input type="password" id="password" placeholder="Password" />
-                <button type="submit">Submit</button>
+                <input 
+                type="text" id="username" placeholder="Username"
+                value={username} onChange={(event) => setUsername(event.target.value)}
+                />
+                <input type="password" id="password" placeholder="Password" 
+                    value={password} onChange={(event) => setUsername(event.target.value)}
+                />
+                <button type="submit">Log in</button>
             </form>
         </div>
 
