3145fab235b30ba0ca057052e5bed5e80e488b3a
sean-code <john.nganga@student.moringaschool.com>
2022-12-06 15:57:35 +0300

    Add useState to return and map the fetched data from the API

--- src/components/home/CarPage.js
+++ src/components/home/CarPage.js
@@ -1,24 +1,49 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import CarList from "./CarList";
+import { useEffect, useState  } from "react";
+import Search from "./Search";
+import CarCard from "./CarCard"
+
 
 function CarPage(){
     const [cars, setCars] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(()=>{
-        fetch("/cars")
-        .then((response) => response.json())
-        .then((carsArray) =>{
-            setCars(carsArray)
-        })
-    }, []);
+    useEffect(() => {
+        fetch('/cars')
+            .then(response => response.json())
+            .then(response => setCars(response));
+
+},[]);
+
+console.log(cars)
+
+
+        //     .then((response) => response.json())
+        //     .then((data) => {
+        //         console.log(data)
+        //     })
+        // });
+
+    // function handleDeleteCar(id) {
+    //     const updatedCarsArray = cars.filter((car) => car.id !== id);
+    //     setCars(updatedCarsArray);
+    //   }
+
+
+    //   const displayedCars = cars.filter((car) => {
+    //     return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+    //   });
 
 
     return(
         <main>
-            <CarList />
-            
+            <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} 
+            />
+                {cars.map((car) => (
+            <CarCard key={car.id} car={car} 
+            />
 
+      ))}  
         </main>
     )
 }
