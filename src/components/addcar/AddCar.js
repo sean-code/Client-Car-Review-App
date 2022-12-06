c7b5754ce77a75bdebfc6af1c3caf406c55c9fea
sean-code <john.nganga@student.moringaschool.com>
2022-12-06 23:30:17 +0300

    import useState and Create adding a new car form

--- src/components/addcar/AddCar.js
+++ src/components/addcar/AddCar.js
@@ -1,9 +1,103 @@
 import React from "react";
+import { useState } from "react";
 
-function AddCar(){
+function AddCar({ onAddCar }){
+    const [make, setMake] = useState(" ");
+    const [model, setModel] = useState(" ");
+    const [fuelType, setFuelType] = useState(" ");
+    const [transmission, setTransmission] = useState(" ");
+    const [mileage, setMileage] = useState(" ");
+    const [engineSize, setEngineSize] = useState(" ");
+    const [year, setYear] = useState(" ");
+    const [image, setImage] = useState(" ");
+
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const car = {
+      make,
+      model,
+      fuelType,
+      transmission,
+      mileage,
+      engineSize,
+      year,
+      image,
+    };
+
+    fetch("/cars", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(car),
+      })
+      .then((response) => response.json())
+      .then((newCar) => {
+        onAddCar(newCar);
+        setMake("");
+        setModel("");
+        setFuelType("");
+        setTransmission("");
+        setMileage("");
+        setEngineSize("");
+        setYear("");
+        setImage("");
+      });
+}
+
     return(
         <>
             <h1>Add New Car</h1>
+            <form>
+                <input 
+                    type="text"
+                    name="name"
+                    placeholder="Car name"
+                />
+
+                <input   
+                    type="text"
+                    name="name"
+                    placeholder="Car name"
+                />
+                
+                <input   
+                    type="text"
+                    name="name"
+                    placeholder="Car name"
+                />
+
+                <input   
+                    type="text"
+                    name="name"
+                    placeholder="Car name"
+                />
+
+                <input   
+                    type="text"
+                    name="name"
+                    placeholder="Car name"
+                />
+
+                <input   
+                    type="text"
+                    name="name"
+                    placeholder="Car name"
+                />
+
+                <input   
+                    type="text"
+                    name="name"
+                    placeholder="Car name"
+                />
+
+                <input   
+                    type="text"
+                    name="name"
+                    placeholder="Car name"
+                />
+            </form>
         </>
     )
 }
