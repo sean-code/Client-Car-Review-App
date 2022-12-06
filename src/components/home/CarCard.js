dd8d30c006c5778d15be7019c0b15ddead67650f
sean-code <john.nganga@student.moringaschool.com>
2022-12-06 10:06:03 +0300

    Add the code to display car information in form of a card

--- src/components/home/CarCard.js
+++ src/components/home/CarCard.js
@@ -1,11 +1,19 @@
 import React from "react";
-const {id, price, make, model, fuel_type, transmission, mileage, engine_size, year, image} = car;
 
-function CarCard(){
+function CarCard({car}){
+    const {id, price, make, model, fuel_type, transmission, mileage, engine_size, year, image} = car;
     return(
-        <>
-
-        </>
+        <li className="card">
+            <img src={image} alt={model} />
+            <h5>{id}</h5>
+            <h4>Make: {make}, {model}</h4>
+            <p>Price: {price}</p>
+            <p>Fuel-type: {fuel_type}</p>
+            <p>Transmission: {transmission}</p>
+            <p>Mileage: {mileage}</p>
+            <p>Engine-size: {engine_size}</p>
+            <p>Year: {year}</p>
+        </li>
     )
 
 }
